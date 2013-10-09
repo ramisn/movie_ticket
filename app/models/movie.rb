1f1fd6bd85433ec857eed89cd34baeec47dfe051
Ramesh K <rameshkror@gmail.com>
2013-10-09 13:58:58 +0530

    updated all

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ActiveRecord::Base
   
-  # tracked owner: ->(controller, model) { controller && controller.current_user }
   attr_accessible :place, :showtime, :title, :image_url
 
   belongs_to :user
@@ -8,4 +7,5 @@ class Movie < ActiveRecord::Base
   def self.search(search)
   find(:all, :conditions => ['place LIKE ?', search])
   end
+
 end
